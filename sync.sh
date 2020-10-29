9f42c02448f92489791d2113afcd7a5a5ad86812
ArDrive App <69483974+ardriveapp@users.noreply.github.com>
2020-10-29 11:17:29 -0400

    Updated user

--- sync.sh
+++ sync.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rsync -r -v -z -e ssh --exclude .git --exclude node_modules --exclude greenlock.d ./ root@blah.blockbin.xyz:~/arweave-dns-gateway
+rsync -r -v -z -e ssh --exclude .git --exclude node_modules --exclude greenlock.d ./ vilenarios@ardrive-dns:~/arweave-dns-gateway
